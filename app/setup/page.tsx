64b4650ea0f17f9c65aee6d44c2fc75c0e717df4
mourad-sat <mouradsat@gmail.com>
2026-08-04 21:33:09 +0200

    feat: add initial setup page

---
+++ app/setup/page.tsx
@@ -0,0 +1,5 @@
+import { AuthForm } from "@/components/AuthForm";
+
+export default function SetupPage() {
+  return <main className="grid min-h-screen place-items-center bg-slate-100 p-4"><AuthForm mode="setup" /></main>;
+}
